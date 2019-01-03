e31759133f21e1cb6a16cdfa5ca73c5595a5bf33
Dilip <dilip_agheda@yahoo.com>
2019-01-03 14:38:15 +1100

    Completed all extra features and finalised style changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+$('article .cell img').on('mouseenter',function(e){
+    let target = e.target;
+    $(target).addClass('animated swing');
+});
+
+$('article .cell img').on('mouseleave',function(e){
+    let target = e.target;
+    $(target).removeClass('animated swing');
+});
